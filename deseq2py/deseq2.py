6b1e963a1a52ef2d049ddcdfe0fe80d7173c296a
LouisFaure <louis.faure75@gmail.com>
2021-10-06 17:59:17 +0200

    added checks and code improvments

--- deseq2py/deseq2.py
+++ deseq2py/deseq2.py
@@ -1,17 +1,56 @@
 import scanpy as sc
-from rpy2.robjects import pandas2ri, Formula
-from rpy2.robjects.packages import importr
-import rpy2.robjects as robjects
+
 import matplotlib.pyplot as plt
 from . import logging as logg
 from . import settings
 
-pandas2ri.activate()
-deseq = importr('DESeq2')
-SE = importr('SummarizedExperiment')
+import shutil
+
+try:
+    from rpy2.robjects import pandas2ri, Formula
+    from rpy2.robjects.packages import importr
+    import rpy2.robjects as robjects
+
+    pandas2ri.activate()
+except Exception as e:
+    raise Exception(
+        "rpy2 installation is necessary for "
+        + task
+        + '. \
+        \nPlease use "pip3 install rpy2" to install rpy2'
+    )
+
+if not shutil.which("R"):
+    raise Exception(
+        "R installation is necessary."
+    )
+
+try:
+    rstats = importr("stats")
+except Exception as e:
+    raise Exception(
+        "R installation is necessary."
+    )
+
+try:
+    SE = importr("SummarizedExperiment")
+except Exception as e:
+    raise Exception(
+        'R package "SummarizedExperiment" is necessary.\n'
+        'Please install it from https://bioconductor.org/packages/release/bioc/html/SummarizedExperiment.html and try again'
+    )    
+    
+    
+try:
+    deseq = importr("DESeq2")
+except Exception as e:
+    raise Exception(
+        'R package "DESeq2" is necessary.\n'
+        'Please install it from https://bioconductor.org/packages/release/bioc/html/DESeq2.html and try again'
+    )
 
 
-def run(adata,formula,top_genes=500,**kwargs):
+def run(adata,formula,get_vsd=True,get_pca=True,top_genes=500,**kwargs):
     
     logg.info("Running DESeq2 pipeline", reset=True, end="\n")
     
@@ -20,25 +59,32 @@ def run(adata,formula,top_genes=500,**kwargs):
                                         colData=adata.obs,
                                         design=Formula(formula))
     dds = deseq.DESeq(dds, **kwargs)
-    adata.layers["normalized"] = deseq.counts_DESeqDataSet(dds, normalized=True).T
+    adata.layers["normalized"] = deseq.counts_DESeqDataSet(dds, normalized=True).T   
     
-    logg.info("    obtaining vsd", end="\n")
+    log_vsd=""
+    if get_vsd:
+        logg.info("    obtaining vsd", end="\n")
+        vsd=deseq.vst(dds, blind=False)
+        adata.layers["vsd"] = SE.assay(vsd).T
+        log_vsd="    .layers['vsd'] variance stabilized count matrix.\n"
     
-    vsd=deseq.vst(dds, blind=False)
-    raw = adata.X.copy()
-    adata.X = SE.assay(vsd).T
+    log_pca=""
+    if get_pca:
+        raw = adata.X.copy() 
+        mat = "vsd" if get_vsd else "normalized"
+        adata.X = adata.layers[mat]
+        adata.var[mat+"_std"]=adata.X.std(axis=0)
+        adata.var["highly_variable"]=False
+        adata.var.loc[adata.var[mat+"_std"].sort_values(ascending=False).index[:top_genes],
+                      "highly_variable"]=True
     
-    adata.var["vsd_std"]=adata.X.std(axis=0)
-    adata.var["highly_variable"]=False
-    adata.var.loc[adata.var["vsd_std"].sort_values(ascending=False).index[:top_genes],
-                  "highly_variable"]=True
-    
-    logg.info("    PCA on highly variable genes", end="\n")
-    
-    sc.pp.pca(adata)
-    
-    adata.layers["vsd"] = adata.X
-    adata.X = raw
+        logg.info("    PCA on highly variable genes using "+mat+ "matrix", end="\n")
+        sc.pp.pca(adata)
+        adata.X = raw
+        log_pca="    .var['"+mat+"_std'] variance of genes calculated from vsd matrix."+\
+            "\n    .var['highly_variable'] genes considered as highly variable."+\
+            "\n    .obsm['X_pca'] PCA results."+\
+            "\n    .uns['pca'] PCA additional results.\n"
     
     adata.uns["dds"] = dds
     
@@ -50,12 +96,8 @@ def run(adata,formula,top_genes=500,**kwargs):
     logg.hint(
         "added\n"
         "    .layers['normalized'] normalized count matrix.\n"
-        "    .layers['vsd'] variance stabilized count matrix.\n"
-        "    .obsm['X_pca'] PCA results.\n"
-        "    .var['vsd_std'] variance of genes calculated from vsd matrix.\n"
-        "    .var['highly_variable'] genes considered as highly variable.\n"
+        +log_vsd+log_pca+
         "    .uns['dds'] DESeq2 R object.\n"
-        "    .uns['pca'] PCA additional results.\n"
         "    .uns['Formula'] formula used for design parameter."
     )
     
@@ -114,6 +156,6 @@ def save(adata,name):
     )
     logg.hint(
         "saved\n"
-        "    "+name+".h5ad anndata file containing counts matrices, pca and DE results.\n"
-        "    "+name+".rds R object of the DESeqDataSet."
+        "    "+name+".h5ad: anndata file containing counts matrices, pca and DE results.\n"
+        "    "+name+".rds: R object of the DESeqDataSet."
     )
